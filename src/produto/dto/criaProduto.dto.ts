e17697b71721916bc758d481fc927b38eab532d9
Juan Pablo <juanpneres15@gmail.com>
2025-05-16 23:26:00 -0300

    validando entrada de dados da criação do produto

---
+++ src/produto/dto/criaProduto.dto.ts
@@ -0,0 +1,54 @@
+import { Type } from "class-transformer";
+import { IsArray, IsDecimal, IsInt, IsNotEmpty, IsNumber, IsPositive, IsUrl, ValidateNested } from "class-validator";
+
+export class CaracteristicaProdutoDTO{
+
+    @IsNotEmpty()
+    nome: String;
+
+    @IsNotEmpty()
+    descricao: String
+} 
+
+export class ImagemProdutoDTO{
+
+    @IsUrl()
+    url: String;
+
+    @IsNotEmpty()
+    descricao: String
+}
+
+export class CriaProdutoDTO{
+
+    @IsNotEmpty()
+    nome: String;
+
+    @IsNumber()
+    @IsPositive()
+    valor: number;
+
+    @IsInt()
+    quantidade: number;
+
+    @IsNotEmpty()
+    descricao: String;
+
+    @ValidateNested()
+    @IsArray()
+    @Type(() => CaracteristicaProdutoDTO)
+    caracteristicas: CaracteristicaProdutoDTO[];
+
+    @ValidateNested()
+    @IsArray()
+    @Type(() => ImagemProdutoDTO)
+    imagem: ImagemProdutoDTO[];
+
+    @IsNotEmpty()
+    categoria: String
+}
+
+function IsNotZero(): (target: CriaProdutoDTO, propertyKey: "valor") => void {
+    throw new Error("Function not implemented.");
+}
+
